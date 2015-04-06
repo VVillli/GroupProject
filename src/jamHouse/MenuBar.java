f03721860d60bb28ea9083fafe70391544eaf070
William Li <willdude3456@gmail.com>
2015-04-06 16:05:42 -0500

    BUBHUBHUBHBUH

--- src/jamHouse/MenuBar.java
+++ src/jamHouse/MenuBar.java
@@ -1,14 +1,18 @@
 package jamHouse;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 public class MenuBar extends JMenuBar{
 	JFrame f;
 	
-	public MenuBar(JFrame f){
+	public MenuBar(FrontEnd f){
 		super();
 		
 		this.f = f;
@@ -23,6 +27,23 @@ public class MenuBar extends JMenuBar{
 		file.add(addUser);
 		file.addSeparator();
 		file.add(close);
+		
+		changeUser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(f.getUsers().length > 0){
+					String[] temp = f.getUsers();
+					String blah = (String)JOptionPane.showInputDialog(f, "Choose User", "Change User", JOptionPane.QUESTION_MESSAGE, null, temp, temp[0]);
+					f.setUser(blah);
+				}
+			}
+		});
+		
+		addUser.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String x = JOptionPane.showInputDialog(f, "USER");
+				f.setUser(x);
+			}
+		});
 		
 		this.add(file);
 	}
