3127ca2db12e246ca88a0f8869713640b6f8c390
Tommy Baxter <tommyjbaxter@gmail.com>
2015-04-06 16:09:17 -0500

    made backend tester

--- src/jamHouse/BackEnd.java
+++ src/jamHouse/BackEnd.java
@@ -42,4 +42,6 @@ public class BackEnd {
 		}
 		return temp;
 	}
+	
+	
 }
---
+++ src/jamHouse/testbois.java
@@ -0,0 +1,12 @@
+package jamHouse;
+
+public class testbois {
+	public testbois(){
+		BackEnd me  =  new BackEnd();
+		me.addMessage("Datasdo", "Kaneki is my waifu");
+		
+	}
+	public static void main(String[] args){
+		new testbois();
+	}
+}
