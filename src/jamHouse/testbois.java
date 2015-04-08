5df8217cdfd83094581783da67290f04cde37430
Tommy Baxter <tommyjbaxter@gmail.com>
2015-04-08 15:17:30 -0500

    messages updated

--- src/jamHouse/testbois.java
+++ src/jamHouse/testbois.java
@@ -3,8 +3,8 @@ package jamHouse;
 public class testbois {
 	public testbois(){
 		BackEnd me  =  new BackEnd();
-		me.addMessage("Datasdo", "Kaneki is my waifu");
-		me.addMessage("Qinsu", "rofl k");
+		me.addMessage("Will", "Kaneki is BAE");
+		me.addMessage("Tommy", "rofl k");
 		me.addMessage("David", "will is dumb");
 	}
 	public static void main(String[] args){
